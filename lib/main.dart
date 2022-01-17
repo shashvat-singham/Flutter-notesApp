ca5575ad7cb222a08fd3f303bf2b43c6664a507a
shashvat singham <shashvat.singham@gmail.com>
2022-01-17 21:47:58 +0530

    initialcommit

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:canton_design_system/canton_design_system.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+
+import 'package:notes_app/src/config/constants.dart';
+import 'package:notes_app/src/ui/views/home_view/home_view.dart';
+import 'dart:async';
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Hive.initFlutter();
+  await Hive.openBox('flutter_notes_app');
+
+  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]).then((_) {
+    runApp(ProviderScope(child: MyApp()));
+  });
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return CantonApp(
+      title: kAppTitle,
+      primaryLightColor: CantonColors.blue,
+      primaryLightVariantColor: CantonColors.blue[200]!,
+      primaryDarkColor: CantonDarkColors.blue,
+      primaryDarkVariantColor: CantonDarkColors.blue[200]!,
+      home: HomeView(),
+    );
+  }
+}
